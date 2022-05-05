64f62b97d283535d9ffef20e6cd96f21e189811e
El7 <enejecaleb@gmail.com>
2022-05-05 22:07:25 +0000

    modulus button & section was added and is functional

--- js/scripts.js
+++ js/scripts.js
@@ -11,6 +11,9 @@ function divide(number5, number6) {
 function multiply(number7, number8) {
     return number7 * number8
 }
+function modulo(number9, number10){
+    return number9 % number10
+}
 
 // user interface logic 
 $(document).ready(function(){
@@ -19,6 +22,7 @@ $(document).ready(function(){
         $("#subtract").hide();
         $("#multiply").hide();
         $("#divide").hide();
+        $("#modulo").hide()
         $("form#myCalculator").submit(function(event){
             event.preventDefault();
             const number1 = parseInt($("#add1").val());
@@ -34,6 +38,7 @@ $(document).ready(function(){
         $("#subtract").show();
         $("#multiply").hide();
         $("#divide").hide();
+        $("#modulo").hide()
         $("form#myCalculator").submit(function (event) {
             event.preventDefault();
             const number3 = parseInt($("#subtract1").val());
@@ -49,6 +54,7 @@ $(document).ready(function(){
         $("#subtract").hide();
         $("#multiply").show();
         $("#divide").hide();
+        $("#modulo").hide()
         $("form#myCalculator").submit(function (event) {
             event.preventDefault()
             const number7 = parseInt($("#multiply1").val());
@@ -64,12 +70,29 @@ $(document).ready(function(){
         $("#subtract").hide();
         $("#multiply").hide();
         $("#divide").show();
+        $("#modulo").hide()
         $("form#myCalculator").submit(function (event) {
             event.preventDefault()
             const number5 = parseInt($("#divide1").val());
             const number6 = parseInt($("#divide2").val());
             const resultant = divide(number5, number6);
             $("#resultant").text(resultant);
+        });
+    });
+});
+$(document).ready(function(){
+    $("#modulo-button").click(function(){
+        $("#add").hide();
+        $("#subtract").hide();
+        $("#multiply").hide();
+        $("#divide").hide();
+        $("#modulo").show()
+        $("form#myCalculator").submit(function (event) {
+            event.preventDefault()
+            const number9 = parseInt($("#modulo1").val());
+            const number10 = parseInt($("#modulo2").val());
+            const conclusion = modulo(number9, number10);
+            $("#conclusion").text(conclusion);
         });
     });
 });
