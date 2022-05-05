ab8de8c14c8a37ec221c35c37981acfd001f2949
El7 <enejecaleb@gmail.com>
2022-05-05 18:22:53 +0000

    addition button/section & subtraction button/section is functional

--- js/scripts.js
+++ js/scripts.js
@@ -14,5 +14,37 @@ function multiply(number7, number8) {
 
 // user interface logic 
 $(document).ready(function(){
-    
+    $("#addition-button").click(function(){
+        $("#add").show();
+        $("#subtract").hide();
+        $("#multiply").hide();
+        $("#divide").hide();
+        $("form#myCalculator").submit(function(event){
+            event.preventDefault();
+            const number1 = parseInt($("#add1").val());
+            const number2 = parseInt($("#add2").val());
+            const result = add(number1, number2);
+            $("#result").text(result);
+        });
+    });
 });
+$(document).ready(function(){
+    $("#sub-button").click(function(){
+        $("#add").hide();
+        $("#subtract").show();
+        $("#multiply").hide();
+        $("#divide").hide();
+        $("form#Calculator").submit(function (event) {
+            event.preventDefault();
+            const number3 = parseInt($("#subtract1").val());
+            const number4 = parseInt($("#subtract2").val());
+            const answer = subtract(number3, number4);
+            $("#answer").text(answer);
+        });
+    });
+});
+$(document).ready(function(){
+    $("#multi-button").click(function(){
+        
+    });
+})
